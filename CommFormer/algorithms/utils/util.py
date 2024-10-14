87c3b365aacf6d3e90c51f7db65bf6f65757763e
archi <1036991178@qq.com>
2024-10-14 14:05:51 +0800

    【模块/问题名称】：添加注释 【变更描述】：给init函数添加注释

--- CommFormer/algorithms/utils/util.py
+++ CommFormer/algorithms/utils/util.py
@@ -5,6 +5,17 @@ import torch
 import torch.nn as nn
 
 def init(module, weight_init, bias_init, gain=1):
+    """初始化网络模块权重
+
+    Args:
+        module (nn.Module): 被初始化的模块
+        weight_init (function): 权重初始化函数，例如nn.init.orthogonal_（正交矩阵初始化权重）
+        bias_init (function): 偏差初始化函数，
+        gain (int, optional): 缩放因子，用于调整初始化的方差。通常对于 ReLU 激活函数设置为 根号2，对于线性激活函数为 1。
+
+    Returns:
+        nn.Module: 完成初始化的模块，即第一个输入参数
+    """
     weight_init(module.weight.data, gain=gain)
     if module.bias is not None:
         bias_init(module.bias.data)
